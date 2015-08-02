bd15bbe0fe7b0b8253d975decf553a36fb205639
Nathan Zadoks <nathan@nathan7.eu>
2015-08-02 00:47:26 -0400

    Allow plain URLs, without .git at the end

--- src/parse-repo.js
+++ src/parse-repo.js
@@ -1,5 +1,5 @@
 module.exports = function(remote, url) {
-    var repoPattern = /github.com(?:[:\/])([^\/]+)\/(.+?)\.git$/;
+    var repoPattern = /github.com(?:[:\/])([^\/]+)\/(.+?)(?:\.git)?$/;
     var result = repoPattern.exec(url);
     if (!result) {
         throw new Error("the remote '" + remote+ "' is not configured to a github repository");
